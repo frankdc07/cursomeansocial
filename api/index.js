b419788c8804bf737810558704d0c0829a1d703b
Moreano Medina Jose Franklyn <moreanj@colpatria.com>
2018-08-24 17:37:58 -0500

    continuacion

--- api/index.js
+++ api/index.js
@@ -1,11 +1,19 @@
 'use strict'
 
 var mongoose = require('mongoose');
+var app = require('./app');
+var port = 3800;
 
+// conexion Database
 mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://localhost:27017/curso_mean_social', { useNewUrlParser: true })
     .then(() => {
-        console.log('Conexion exitosa :)');
+        console.log('Conexion exitosa a la BD :)');
+
+        //crear servidor
+        app.listen(port, ()=>{
+            console.log('Servidor corriendo en http://localhost:3800');
+        });
     })
     .catch(err => console.log(err));
 
