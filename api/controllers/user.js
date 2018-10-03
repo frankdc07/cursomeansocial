141498260bc3b0e10853c55988504637ceec8ced
frankdc0 <jf.moreano89@uniandes.edu.co>
2018-10-02 21:50:52 -0500

    avance 20181002

--- api/controllers/user.js
+++ api/controllers/user.js
@@ -1,9 +1,12 @@
 'use strict'
 
 var bcrypt = require('bcrypt-nodejs');
+var mongoosePaginate = require('mongoose-pagination');
 
 var User = require('../models/user');
+var jwt = require('../services/jwt');
 
+// Métodos de prueba
 function home(req, res){
     res.status(200).send({
         message: 'Hola mundo desde el servidor de NodeJS'
@@ -17,6 +20,7 @@ function pruebas(req, res){
     });
 }
 
+// Registro
 function saveUser(req, res){
     var params = req.body;
     var user = new User();
@@ -40,23 +44,22 @@ function saveUser(req, res){
 
                             if(users && users.length >= 1){
                                 return res.status(200).send({message: 'El usuario que intentas registrar ya existe'});
+                            }else{
+                                //Cifra el password y guarda los datos
+                                bcrypt.hash(params.password, null, null, (err, hash) => {
+                                    user.password = hash;
+                                    user.save((err, userStored) => {
+                                        if(err) return res.status(500).send({message: 'Error al guardar el usuario'});
+
+                                        if(userStored){
+                                            res.status(200).send({user: userStored});
+                                        }else{
+                                            res.status(404).send({message: 'No se ha registrado el usuario'});
+                                        }
+                                    });
+                                });
                             }
             });
-
-            //Cifra el password y guarda los datos
-            bcrypt.hash(params.password, null, null, (err, hash) => {
-                user.password = hash;
-                user.save((err, userStored) => {
-                    if(err) return res.status(500).send({message: 'Error al guardar el usuario'})
-
-                    if(userStored){
-                        res.status(200).send({user: userStored});
-                    }else{
-                        res.status(404).send({message: 'No se ha registrado el usuario'});
-                    }
-                });
-            });
-
     }else{
         res.status(200).send({
             message: 'Envía todos los campos necesarios!!'
@@ -64,8 +67,107 @@ function saveUser(req, res){
     }
 }
 
+// Login
+function loginUser(req, res){
+    var params = req.body;
+    var email = params.email;
+    var password = params.password;
+
+    User.findOne({email: email}, (err, user) => {
+        if(err) return res.status(500).send({message: 'Error en la petición'});
+
+        if(user){
+            bcrypt.compare(password, user.password, (err, check) => {
+                if (check){
+                    if(params.gettoken){
+                        //generar y devolver token
+                        return res.status(200).send({
+                            token: jwt.createToken(user)
+                        });
+                    }else{
+                        //devolver datos del usuario
+                        user.password = undefined;
+                        return res.status(200).send({user});
+                    }
+                }else{
+                    return res.status(400).send({message: 'El usuario no se ha podido identificar'});
+                }
+            });
+        }else{
+            return res.status(400).send({message: 'El usuario no se ha podido identificar!!'});
+        }
+    });
+
+}
+
+// Conseguir datos de un usuario
+function getUser(req, res){
+    var userId = req.params.id;
+
+    User.findById(userId, (err, user) => {
+        if(err) return res.status(500).send({message: 'Error en la petición'});
+
+        if(!user) return res.status(404).send({message: 'El usuario no existe'});
+
+        user.password = undefined;
+        return res.status(200).send({user});
+    });
+}
+
+// Devolver un listado de usuarios paginado
+function getUsers(req,res){
+    var identity_user_id = req.user.sub;
+
+    var page = 1;
+
+    if(req.params.page){
+        page = req.params.page;
+    }
+
+    var itemsPerPage = 5;
+
+    User.find().sort('_id').paginate(page, itemsPerPage, (err, users, total) => {
+        if(err) return res.status(500).send({message: 'Error en la petición'});
+
+        if(!users.length) return res.status(404).send({message: 'No hay usuarios disponibles'});
+
+        return res.status(200).send({
+            users,
+            total,
+            pages: Math.ceil(total/itemsPerPage)
+        });
+
+    });
+}
+
+// Edición de datos de usuarios
+function updateUser(req, res){
+    var userID = req.params.id;
+    var update = req.body;
+
+    // borrar la propiedad password
+    delete update.password;
+
+    if(userID != req.user.sub){
+        return res.status(500).send({message: 'No tienes permiso para actualizar los datos del usuario'});
+    }
+
+    User.findByIdAndUpdate(userID, update, {new: true}, (err, userUpdated) => {
+        if(err) return res.status(500).send({message: 'Error en la petición'});
+
+        if(!userUpdated) return res.status(404).send({message: 'No se ha podido actualizar el usuario'});
+
+        return res.status(200).send({user: userUpdated});
+    });
+
+}
+
 module.exports = {
     home,
     pruebas,
-    saveUser
+    saveUser,
+    loginUser,
+    getUser,
+    getUsers,
+    updateUser
 }
